38e5ef09cafb25b51deb348e1f8a40affdebb297
LukeJohnson03 <lukejohnsonlgj905@gmail.com>
2025-08-05 15:35:04 +0200

    Emoji picker modal set up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,21 @@
 import { View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-
 import { useState } from 'react';
-
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-import CircleButton from '@/components/CircleButton';
 import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+
+import EmojiPicker from '@/components/EmojiPicker';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -35,11 +37,15 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-    // Implement Later
+    setIsModalVisible(true);
   };
 
-  const onSaveImageAsync = () => {
-    // Implement Later
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
   };
 
   return (
@@ -48,7 +54,7 @@ export default function Index() {
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
-         <View style={styles.optionsContainer}>
+        <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
             <IconButton icon="refresh" label="Reset" onPress={onReset} />
             <CircleButton onPress={onAddSticker} />
@@ -61,6 +67,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* Emoji list component will go here */}
+      </EmojiPicker>
     </View>
   );
 }
